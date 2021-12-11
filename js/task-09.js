4ffaec6f684d3a475db9a8fa58b7ba4b2f558ec7
Denis Venher <vendenven@gmail.com>
2021-12-11 18:24:51 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -12,3 +12,16 @@ function onChangeBtnClick() {
   document.body.style.backgroundColor = color;
   spanEl.textContent = color;
 };
+
+// const bodyEl = document.querySelector('body');
+// const btn = document.querySelector('.change-color');
+// const colorBaground = document.querySelector('.color');
+// btn.addEventListener('click', change_color);
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// function change_color() {
+//   const colorspan = getRandomHexColor();
+//   bodyEl.style.backgroundColor = colorspan;
+//   colorBaground.textContent = colorspan;
+// }
